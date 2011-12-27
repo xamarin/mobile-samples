ad8836c157c4ce7f443b9c75dd6eb9a46e44dd6e
ConceptDev <craig.dunn@gmail.com>
2011-12-27 20:18:01 +1100

    Split custom elements cs files into two
    Maybe make merging easier, with one-class-per-file

--- MWC/MWC.iOS/UI/CustomElements/SpeakerElement.cs
+++ MWC/MWC.iOS/UI/CustomElements/SpeakerElement.cs
@@ -7,73 +7,15 @@ using MWC.BL;
 
 namespace MWC.iOS.UI.CustomElements
 {
-	public class SpeakerCell : UITableViewCell {
-		static UIFont bigFont = UIFont.BoldSystemFontOfSize (16);
-		static UIFont midFont = UIFont.BoldSystemFontOfSize (15);
-		static UIFont smallFont = UIFont.SystemFontOfSize (14);
-		UILabel bigLabel, smallLabel;
-		Speaker Speaker;
-		const int ImageSpace = 32;
-		const int Padding = 8;
-		
-		public SpeakerCell (UITableViewCellStyle style, NSString ident, Speaker Speaker, string big, string small) : base (style, ident)
-		{
-			SelectionStyle = UITableViewCellSelectionStyle.Blue;
-			
-			bigLabel = new UILabel () {
-				TextAlignment = UITextAlignment.Left,
-				BackgroundColor = UIColor.FromWhiteAlpha (0f, 0f)
-			};
-			smallLabel = new UILabel () {
-				TextAlignment = UITextAlignment.Left,
-				Font = smallFont,
-				TextColor = UIColor.DarkGray,
-				BackgroundColor = UIColor.FromWhiteAlpha (0f, 0f)
-			};
-			UpdateCell (Speaker, big, small);
-			
-			ContentView.Add (bigLabel);
-			ContentView.Add (smallLabel);
-		}
-		
-		public void UpdateCell (Speaker Speaker, string big, string small)
-		{
-			this.Speaker = Speaker;
-			
-			bigLabel.Font = big.Length > 35 ? midFont : bigFont;
-			bigLabel.Text = big;
-			
-			smallLabel.Text = small;
-		}
-
-		public override void LayoutSubviews ()
-		{
-			base.LayoutSubviews ();
-			var full = ContentView.Bounds;
-			var bigFrame = full;
-			
-			bigFrame.Height = 22;
-			bigFrame.X = Padding;
-			bigFrame.Width -= ImageSpace+Padding;
-			bigLabel.Frame = bigFrame;
-			
-			var smallFrame = full;
-			smallFrame.Y = 22;
-			smallFrame.Height = 21;
-			smallFrame.X = Padding;
-			smallFrame.Width = bigFrame.Width;
-			smallLabel.Frame = smallFrame;
-		}
-	}
-	
 	// Renders a Speaker
-	public class SpeakerElement : Element {
+	public class SpeakerElement : Element 
+	{
 		static NSString key = new NSString ("SpeakerElement");
 	
 		Speaker Speaker;
 		string subtitle;
 		
-		public SpeakerElement (Speaker Speaker) : base (Speaker.Title)
+		public SpeakerElement (Speaker Speaker) : base (Speaker.Name)
 		{
 			this.Speaker = Speaker;
 			if(String.IsNullOrEmpty(Speaker.Title))
