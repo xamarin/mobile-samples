2ccc49b348935a8b9c1afeb050b837b79bd37c13
Brian Kim <brian.kim@xamarin.com>
2012-01-17 17:19:22 -0800

    adding config to parse either sessions/speakers, exhibitors, or both.

--- MWC/MWC.Parser/ParserApp.cs
+++ MWC/MWC.Parser/ParserApp.cs
@@ -12,18 +12,31 @@ namespace MWC.Parser
 	public class ParserApp
 	{
 		static string parseMode = ConfigurationManager.AppSettings["parseMode"];
+		static string parseItems = ConfigurationManager.AppSettings["parseItems"];
 
 		public void WriteConferenceXML(string path)
 		{
+			if(parseItems == "sessions" || parseItems == "both")
+			{
+				DoSessions(path);
+			}
+			if(parseItems == "exhibitors" || parseItems == "both")
+			{
+				DoExhibitors(path);
+			}
+
+		}
+
+		private void DoSessions(string path)
+		{
 			bool doPartial = parseMode == "partial";
 
-			Console.WriteLine("Starting the parser...");
+			Console.WriteLine("Getting sessions/speakers/");
 			Conference conf = ConferenceManager.GetConference(doPartial);
-
 			Console.WriteLine(string.Concat(conf.Sessions.Count, " sessions"));
 			Console.WriteLine(string.Concat(conf.Speakers.Count, " speakers"));
-			//Console.WriteLine(string.Concat(conf.Exhibitors.Count, " exhibitors"));
 
+			Console.WriteLine("Writing output");
 			using(TextWriter tw = File.CreateText(Path.Combine(path, "Conference.xml")))
 			{
 				XmlSerializer serializer = new XmlSerializer(typeof(Conference));
@@ -34,10 +47,17 @@ namespace MWC.Parser
 			{
 				tw.Write(DateTime.Now.ToString());
 			}
+		}
 
+		private void DoExhibitors(string path)
+		{
+			bool doPartial = parseMode == "partial";
+
+			Console.WriteLine("Getting exhibitors");
 			Exhibitors exhibitors = new Exhibitors();
 			exhibitors.Items = ExhibitorManager.GetExhibitorList(doPartial);
-
+			Console.WriteLine(string.Concat(exhibitors.Items.Count, " exhibitors"));
+			Console.WriteLine("Writing output");
 			using(TextWriter tw = File.CreateText(Path.Combine(path, "Exhibitors.xml")))
 			{
 				XmlSerializer serializer = new XmlSerializer(typeof(Exhibitors));
@@ -48,8 +68,6 @@ namespace MWC.Parser
 			{
 				tw.Write(DateTime.Now.ToString());
 			}
-
-
 		}
 	}
 }
