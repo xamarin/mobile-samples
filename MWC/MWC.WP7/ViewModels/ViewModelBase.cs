ddbb98cf88b0ada4bbd44204d3de98190e61797e
Frank A. Krueger <fak@praeclarum.org>
2012-01-24 12:26:56 -0800

    Added function to clean up plaintext entries to remove redundant spacing

--- MWC/MWC.WP7/ViewModels/ViewModelBase.cs
+++ MWC/MWC.WP7/ViewModels/ViewModelBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Collections.Generic;
 
 namespace MWC.WP7.ViewModels
 {
@@ -13,6 +14,33 @@ namespace MWC.WP7.ViewModels
             if (handler != null) {
                 handler (this, new PropertyChangedEventArgs (propertyName));
             }
+        }
+
+        protected string CleanupPlainTextDocument (string doc)
+        {
+            if (string.IsNullOrWhiteSpace (doc)) {
+                return "";
+            }
+
+            //
+            // We want to make sure that there are no more than 1
+            // blank continuous lines
+            //
+            var oldLines = doc.Split ('\n');
+            var newLines = new List<string> ();
+
+            var prevWasBlank = true;
+            foreach (var line in oldLines) {
+                var blank = string.IsNullOrWhiteSpace (line);
+
+                if (!blank || !prevWasBlank) {
+                    newLines.Add (line.TrimEnd ());
+                }
+
+                prevWasBlank = blank;
+            }
+
+            return string.Join ("\r\n", newLines);
         }
     }
 }
