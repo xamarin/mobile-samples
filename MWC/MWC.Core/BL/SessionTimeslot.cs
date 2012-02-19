6db0d4219714d5b6824cf9f14401addb3f2ab0fd
Craig Dunn <craig.dunn@gmail.com>
2012-02-20 10:52:19 +1100

    Core: rename Core.MT to just plain "Core" to indicate it is shared

---
+++ MWC/MWC.Core/BL/SessionTimeslot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MWC.BL {
+    public class SessionTimeslot {
+		/// <summary>
+		/// Used to group sessions (inc favorites) by time, esp in a linq expression for MT.D
+		/// </summary>
+        public SessionTimeslot(string timeslot, IEnumerable<Session> sessions)
+        {
+            Timeslot = timeslot;
+            Sessions = new List<Session>(sessions);
+        }
+        public string Timeslot { get; set; }
+        public IList<Session> Sessions { get; set; }
+    }
+}
