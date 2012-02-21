c7a469076b5d84e6b8eb5417e0fc8618ee1af296
Craig Dunn <craig.dunn@gmail.com>
2012-02-21 14:33:54 +1100

    Android: add icons/links to About

--- MWC/MWC.Core/Constants.cs
+++ MWC/MWC.Core/Constants.cs
@@ -29,7 +29,10 @@ namespace MWC {
 		public const string AboutUrlTwitter = "https://twitter.com/#!/xamarinhq";
 		public const string AboutUrlYouTube = "http://www.youtube.com/xamarinhq";
 		public const string AboutUrlFacebook = "http://www.facebook.com/pages/Xamarin/283148898401104";
+        public const string AboutUrlBlogRss = "http://blog.xamarin.com/feed/";
+        public const string AboutMailto = "hello@xamarin.com";
+        public const string AboutTwitter = "@xamarinhq";
 
-		public const string AboutText = @"Xamarin was founded in 2011 with the mission to make it fast, easy and fun to build great mobile apps. Xamarin’s products are used by individual developers and companies, including VMware, Target, Rdio, Medtronic and Unity Technologies, to simplify creation and operation of high-performance, cross-platform mobile consumer and corporate applications, targeting phones and tablets running iOS, Android and Windows. For more information, visit http://xamarin.com.";
+		public const string AboutText = @"Xamarin was founded in 2011 with the mission to make it fast, easy and fun to build great mobile apps. Xamarin’s products are used by individual developers and companies, including VMware, Target, Rdio, Medtronic and Unity Technologies, to simplify creation and operation of high-performance, cross-platform mobile consumer and corporate applications, targeting phones and tablets running iOS, Android and Windows. For more information, visit http://xamarin.com or email hello@xamarin.com.";
 	}
 }
--- MWC/MWC.Droid/Screens/About/AboutXamScreen.cs
+++ MWC/MWC.Droid/Screens/About/AboutXamScreen.cs
@@ -1,6 +1,8 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Widget;
+using Android.Content;
 
 namespace MWC.Android.Screens
 {
@@ -13,6 +15,40 @@ namespace MWC.Android.Screens
 
             // set our layout to be the home screen
             this.SetContentView(Resource.Layout.AboutXamScreen);
+
+            var txt = FindViewById<TextView>(Resource.Id.AboutTextView);
+            txt.Text = Constants.AboutText;
+
+            var so = FindViewById<ImageView>(Resource.Id.StackOverflowImage);
+            so.Click += (object sender, System.EventArgs e) => {
+                OpenUrl(Constants.AboutUrlStackOverflow);
+            };
+            var li = FindViewById<ImageView>(Resource.Id.LinkedInImage);
+            li.Click += (object sender, System.EventArgs e) => {
+                OpenUrl(Constants.AboutUrlLinkedIn);
+            };
+            var tw = FindViewById<ImageView>(Resource.Id.TwitterImage);
+            tw.Click += (object sender, System.EventArgs e) => {
+                OpenUrl(Constants.AboutUrlTwitter);
+            };
+            var yt = FindViewById<ImageView>(Resource.Id.YouTubeImage);
+            yt.Click += (object sender, System.EventArgs e) => {
+                OpenUrl(Constants.AboutUrlTwitter);
+            };
+            var fb = FindViewById<ImageView>(Resource.Id.FacebookImage);
+            fb.Click += (object sender, System.EventArgs e) => {
+                OpenUrl(Constants.AboutUrlFacebook);
+            };
+            var rs = FindViewById<ImageView>(Resource.Id.RssImage);
+            rs.Click += (object sender, System.EventArgs e) => {
+                OpenUrl(Constants.AboutUrlBlogRss);
+            };
+        }
+
+        void OpenUrl (string url)
+        {
+            Intent browserIntent = new Intent(Intent.ActionView, global::Android.Net.Uri.Parse(url));
+            StartActivity(browserIntent); 
         }
     }
 }
