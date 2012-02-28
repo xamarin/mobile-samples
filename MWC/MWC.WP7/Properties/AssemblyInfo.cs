e2cae89eb0ad034bb64ae572c550d5b1f33cd189
Craig Dunn <craig.dunn@gmail.com>
2012-02-29 08:26:58 +1100

    WP7: bump version to 1.1

--- MWC/MWC.WP7/Properties/AssemblyInfo.cs
+++ MWC/MWC.WP7/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MWC.WP7")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Mobile World Congress (unofficial)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Xamarin Inc")]
 [assembly: AssemblyProduct("MWC.WP7")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
