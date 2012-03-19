ccefe9377c7477880c65e92d30f26b39ace06029
Craig Dunn <craig.dunn@gmail.com>
2012-03-19 17:02:51 +1100

    Android: unfortunately need a compiler directive #if ANDROID because Apple doesn't like sqlite files in Environment.SpecialFolder.Pesonal location)

--- MWC/MWC.Core/DL/MwcDatabase.cs
+++ MWC/MWC.Core/DL/MwcDatabase.cs
@@ -54,10 +54,15 @@ namespace MWC.DL {
 #if SILVERLIGHT
             var path = "MwcDB.db3";
 #else
+
+#if __ANDROID__
+            string libraryPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); ;
+#else
 			// we need to put in /Library/ on iOS5.1 to meet Apple's iCloud terms
 			// (they don't want non-user-generated data in Documents)
 			string documentsPath = Environment.GetFolderPath (Environment.SpecialFolder.Personal); // Documents folder
 			string libraryPath = Path.Combine (documentsPath, "../Library/");
+#endif
 			var path = Path.Combine (libraryPath, "MwcDB.db3");
 #endif		
 			return path;	
