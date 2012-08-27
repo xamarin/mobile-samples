0ba718c04eec2991e6b0f04415e5055454fe034b
Craig Dunn <craig.dunn@conceptdevelopment.net>
2012-08-27 11:28:23 +1000

    [ContentControls] combine iOS & Android into one sample, for cross-platform devs to discover

---
+++ ContentControls/AndroidContentControls/Screens/Maps/MapViewAnnotationScreen.cs
@@ -0,0 +1,77 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Webkit;
+#if !NOGOOGLEMAP
+using Android.GoogleMaps; // Added this reference to project manually - Maps isn't "built in"
+#endif
+
+namespace ContentControls {
+
+#if NOGOOGLEMAP
+	[Activity(Label = "MapViewAnnotation")]
+	public class MapViewAnnotationScreen : Activity {
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView(Resource.Layout.NoGoogleMaps);
+		}
+	}
+#else
+    [Activity(Label = "MapViewAnnotation")]
+    public class MapViewAnnotationScreen : MapActivity {  // NOTE: subclasses MapActivity (and implement IsRouteDisplayed)
+        MapView map;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.MapView);
+
+            map = FindViewById<MapView>(Resource.Id.Map);
+
+            map.Clickable = true;
+            map.Traffic = false;
+            map.Satellite = true;
+
+            map.SetBuiltInZoomControls(true);
+            map.Controller.SetZoom(15);
+            map.Controller.SetCenter(new GeoPoint((int)(34.120 * 1e6), (int)(-118.188 * 1e6))); // Los Angeles
+
+            AddPinOverlay(map);
+        }
+        
+        // REQUIRED for pin
+        void AddPinOverlay(MapView map)
+        {
+            var pin = Resources.GetDrawable(Resource.Drawable.map_pin);
+            var pinOverlay = new MapPinOverlay(pin);
+            map.Overlays.Add(pinOverlay);
+        }
+
+        // REQUIRED by MapActivity
+        protected override bool IsRouteDisplayed
+        {
+            get { return false; }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            menu.Add("Satellite");
+            menu.Add("Road");
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.TitleFormatted.ToString()) {
+            case "Road":
+                map.Satellite = false; break;
+            default:
+                map.Satellite = true; break;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+#endif
+}
---
+++ ContentControls/README.md
@@ -0,0 +1,50 @@
+Content Controls
+================
+iOS
+---
+Illustrates simple and advanced usage of the following content
+controls: Web View, Map View, Search  Controller, Navigation Bar,
+and Tab Bar Controller.
+
+**Web View** - Contains a simple web browser built around the Web
+View control. Illustrates how to load URLs, hook into events,
+perform browsing functions, etc. Also has a web view view that
+loads HTML content from a local directory. Additionally, shows
+how to interact with content in the web view by executing JavaScript
+and listening for specially formed URLs, in order to execute code
+outside of the browser.
+
+**Maps** - Illustrates how to display a map using the map view. Includes
+traditional map, satellite, and hybrid layers. Also shows how to add
+annotations and overlays to the map, as well as calculate the distance
+to particular locations from the map center.
+
+**Search** - Illustrates how to use the search bar and listen for events to
+provide real-time search results as the search text changes. 
+
+**Navigation Bar** - Illustrates how to customize the navigation bar by
+adding buttons, changing the color, making it transparent, and event
+adding a toolbar at the bottom of the view. 
+
+**Tab Bar Controller** - Illustrates adding items to the tab bar controller
+as well as allowing it to be edited/customized by rearranging tab items
+and adding/removing them.
+Android
+-------
+This sample demonstrates Android's content controls.
+
+**Browsers** - WebView, WebView Browser, Local Content, Generated Content, Javascript Interop
+
+**Maps** ^^ - Basic MapView, MapView with Annotation, MapView with Overlay
+
+**Search** - AutoCompleteTextView
+
+**Nav** - Activity Fade, Activity Zoom, Light Theme
+
+^^ **NOTE**: the Map examples require the PrivateStrings.xml "GoogleMapAPIKey" key to be uncommented and a valid key added. See
+http://docs.xamarin.com/android/advanced_topics/Obtaining_a_Google_Maps_API_Key (you should also have installed the Google APIs Add-on
+via the Android SDK Manager).
+
+Authors
+-------
+Bryan Costanich, Craig Dunn
