d3f91f0b544c46d0eda507cb0a5e8d3ca0239798
Craig Dunn <craig.dunn@conceptdevelopment.net>
2013-08-15 13:35:58 -0700

    [AsyncAwait] sample to go with xamarin 8/15

---
+++ AsyncAwait/iOS/ComparisonsOldvsNew/iOSViewController_old.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using MonoTouch.AssetsLibrary;
+using System.Collections.Generic;
+using System.Threading;
+/*
+
+AsyncAwait : C# old-style 
+
+*/
+namespace iOS
+{
+	public partial class iOSViewController_old : UIViewController
+	{
+		string localPath;
+		public iOSViewController_old (IntPtr handle) : base (handle)
+		{
+		}
+
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			GetButton.TouchUpInside += HandleTouchUpInside;
+		}
+
+
+		delegate void HomepageDownloaded (int length);		
+		HomepageDownloaded downloaded;
+		void HandleTouchUpInside (object sender, EventArgs e)
+		{
+			ResultLabel.Text = "loading...";
+			ResultTextView.Text = "loading...\n";
+			DownloadedImageView.Image = null;
+
+			int intResult;
+			downloaded += (len) => {
+				intResult = len;
+				ResultLabel.Text = "Length: " + intResult ;
+			};
+			DownloadHomepage();
+		}
+
+
+		public void DownloadHomepage()
+		{
+			var webClient = new WebClient();
+
+			webClient.DownloadStringCompleted += (sender, e) => {
+				if(e.Cancelled || e.Error != null) {
+					// do something with error
+				}
+				string contents = e.Result;
+
+				int length = contents.Length;
+				InvokeOnMainThread (() => {
+					ResultTextView.Text += "Downloaded the html and found out the length.\n\n";
+				});
+				webClient.DownloadDataCompleted += (sender1, e1) => {
+					if(e1.Cancelled || e1.Error != null) {
+						// do something with error
+					}
+					SaveBytesToFile(e1.Result, "team.jpg");
+					
+					InvokeOnMainThread (() => {
+						ResultTextView.Text += "Downloaded the image.\n";
+						DownloadedImageView.Image = UIImage.FromFile (localPath);
+					});
+
+					ALAssetsLibrary library = new ALAssetsLibrary();     
+					var dict = new NSDictionary();
+					library.WriteImageToSavedPhotosAlbum (DownloadedImageView.Image.CGImage, dict, (s2,e2) => {
+						InvokeOnMainThread (() => {
+							ResultTextView.Text += "Saved to album assetUrl\n";
+						});
+						if (downloaded != null)
+							downloaded(length);
+					});
+				};
+				webClient.DownloadDataAsync(new Uri("http://xamarin.com/images/about/team.jpg"));
+			};
+
+			webClient.DownloadStringAsync(new Uri("http://xamarin.com/"));
+		}
+
+		void SaveBytesToFile(byte[] r, string f)
+		{
+			string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			string localFilename = f;
+			localPath = Path.Combine (documentsPath, localFilename);
+			File.WriteAllBytes (localPath, r); // writes to local storage   
+		}
+
+		#region irrelevant
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		{
+			// Return true for supported orientations
+			return (toInterfaceOrientation == UIInterfaceOrientation.Portrait);
+		}
+
+		[Outlet]
+		MonoTouch.UIKit.UIImageView DownloadedImageView { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton GetButton { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel ResultLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextView ResultTextView { get; set; }
+
+		[Action ("UIButton9_TouchUpInside:")]
+		partial void UIButton9_TouchUpInside (MonoTouch.UIKit.UIButton sender);
+		#endregion
+	}
+}
+
---
+++ AsyncAwait/iOS/iOSViewController.designer.cs
@@ -0,0 +1,53 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace iOS
+{
+	[Register ("iOSViewController")]
+	partial class iOSViewController
+	{
+		[Outlet]
+		MonoTouch.UIKit.UIImageView DownloadedImageView { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton GetButton { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel ResultLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextView ResultTextView { get; set; }
+
+		[Action ("UIButton9_TouchUpInside:")]
+		partial void UIButton9_TouchUpInside (MonoTouch.UIKit.UIButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (DownloadedImageView != null) {
+				DownloadedImageView.Dispose ();
+				DownloadedImageView = null;
+			}
+
+			if (GetButton != null) {
+				GetButton.Dispose ();
+				GetButton = null;
+			}
+
+			if (ResultLabel != null) {
+				ResultLabel.Dispose ();
+				ResultLabel = null;
+			}
+
+			if (ResultTextView != null) {
+				ResultTextView.Dispose ();
+				ResultTextView = null;
+			}
+		}
+	}
+}
