1cac1dba63aafe3b195cc31edcf80598016219e7
Tom Opgenorth <tom@opgenorth.net>
2013-09-09 16:14:11 -0600

    Matching up the code in this repo with the changes from the docs repo
    Docs Repo commit:77279d278b762c888028cbe42693156db76000d7
    
    Relevant post on the forums: http://forums.xamarin.com/discussion/7708/async-await-sample

--- AsyncAwait/Droid/MainActivity.cs
+++ AsyncAwait/Droid/MainActivity.cs
@@ -60,10 +60,10 @@ namespace Droid
 
 			Task<string> contentsTask = httpClient.GetStringAsync("http://xamarin.com"); // async method!
 
-			ResultEditText.Text += "DownloadHomepage method continues after async call. . . . .\n";
 
 			// await! control returns to the caller and the task continues to run on another thread
 			string contents = await contentsTask;
+			ResultEditText.Text += "DownloadHomepage method continues after async call. . . . .\n";
 
 			// After contentTask completes, you can calculate the length of the string.
 			int exampleInt = contents.Length;
