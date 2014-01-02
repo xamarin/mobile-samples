c22ef43278ea76786eb3de1aadefc65c0ee7b3eb
Peter Collins <peter.collins@xamarin.com>
2014-01-02 14:07:09 -0500

    [AsyncAwait] Update the imageview after async download in android sample

--- AsyncAwait/Droid/MainActivity.cs
+++ AsyncAwait/Droid/MainActivity.cs
@@ -8,6 +8,7 @@ using Android.OS;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.IO;
+using Android.Graphics;
 
 namespace Droid
 {
@@ -77,12 +78,16 @@ namespace Droid
 
 			string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
 			string localFilename = "team.jpg";
-			string localPath = Path.Combine (documentsPath, localFilename);
+			string localPath = System.IO.Path.Combine (documentsPath, localFilename);
 			File.WriteAllBytes (localPath, imageBytes); // writes to local storage   
 
 			ResultTextView.Text += "Downloaded the image.\n";
 
-			// ImageView stuff goes here
+			var localImage = new Java.IO.File (localPath);
+			if (localImage.Exists ()) {
+				var teamBitmap = BitmapFactory.DecodeFile (localImage.AbsolutePath);
+				DownloadedImageView.SetImageBitmap (teamBitmap);
+			}
 
 			ResultEditText.Text += contents; // just dump the entire HTML
 			return exampleInt; // Task<TResult> returns an object of type TResult, in this case int
