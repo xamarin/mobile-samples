176138a4286910b1c4da513c84ab933fee4b2b7d
olegoid <olegoid91@gmail.com>
2014-01-24 03:38:19 +0400

    [MWC.Core] Update twitter parser with ability to work with JSON data sent by twitter API v1.1

--- MWC/MWC.Core/SAL/TwitterParser.cs
+++ MWC/MWC.Core/SAL/TwitterParser.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Net;
 using System.Diagnostics;
 using MWC.BL;
+using System.IO;
+using System.Json;
+using System.Globalization;
 
 namespace MWC.SAL
 {	
@@ -15,44 +18,64 @@ namespace MWC.SAL
 	/// Inspired by the RSSRepository from 
 	/// http://softwareandservice.wordpress.com/2009/09/21/building-a-rss-reader-iphone-app-using-monotouch/
 	/// </remarks>
-	public class TwitterParser<Tweet> : XmlFeedParserBase<MWC.BL.Tweet>
+	public class TwitterParser<Tweet> : FeedParserBase<MWC.BL.Tweet>
 	{	
+		private TwitterAuthorizer twitterAuthorizer;
 		//https://dev.twitter.com/docs/api/1/get/search
-		const string fileName = "TwitterFeed.xml";
+		const string fileName = "TwitterFeed.json";
 		//static string url = "http://search.twitter.com/search.atom?q=%40mobileworldlive&show-user=true&rpp=20";
 		const string urlFormat = "http://search.twitter.com/search.atom?q={0}&show-user=true&rpp={1}";
 		
 		public TwitterParser(string url) : base(url, fileName)
 		{
+			twitterAuthorizer = new TwitterAuthorizer ();
 			Debug.WriteLine ("Created new Twitter Repository");
 		}
 		
 		public TwitterParser(string keyWord, string amountOfTweets) : base(String.Format(urlFormat, keyWord, amountOfTweets), fileName)
 		{
+			twitterAuthorizer = new TwitterAuthorizer ();
 			Debug.WriteLine ("setting up twitter repository");
 		}
-		
-		protected override List<MWC.BL.Tweet> ParseXml (string xml)
+
+		public override void Refresh (Action action)
 		{
-			Debug.WriteLine ("Starting Parsing XML");
+			var webClient = new WebClient ();
+			Debug.WriteLine ("Get remote json data");
+			webClient.DownloadStringCompleted += (sender, e) =>
+			{
+				try {
+					SaveLocal (e.Result);
+					var items = Parse (e.Result);
+				} catch (Exception ex) {
+					Debug.WriteLine ("ERROR saving downloaded JSON: " + ex);
+				}
+				action();
+			};
 
-			XNamespace ns = "http://www.w3.org/2005/Atom";
-			XDocument rssFeed = XDocument.Parse(xml);
-			var items = 
-				from item in rssFeed.Descendants(ns + "entry")
-				select new MWC.BL.Tweet
-				{
-					Title  = item.Element(ns + "title").Value,
-					Content = item.Element(ns + "content").Value,
-					Author = item.Element(ns + @"author").Value,
-					Published = DateTime.Parse (item.Element(ns + "published").Value),
-					ImageUrl = item.Elements(ns + "link")
-									.Where (x => x.Attribute("rel").Value == "image")
-									.Select (x => x.Attribute ("href").Value).First ()		
-				};
-			Debug.WriteLine ("Finished Parsing Tweet XML");
+			webClient.Headers.Set ("Authorization", string.Format ("{0} {1}", twitterAuthorizer.AuthenticationInfo.TokenType,
+				twitterAuthorizer.AuthenticationInfo.AccessToken));
+			webClient.Encoding = System.Text.Encoding.UTF8;webClient.DownloadStringAsync (new Uri(Constants.TwitterUrl));
+		}
+		
+		protected override List<MWC.BL.Tweet> Parse (string data)
+		{
+			Debug.WriteLine ("Starting Parsing JSON");
 
-			return items.ToList();
+			var tweets = new List<MWC.BL.Tweet> ();
+			var statusesJson = JsonValue.Parse (data)["statuses"];
+			foreach(var jsonItem in statusesJson) {
+				var tweet = jsonItem as JsonObject;
+				tweets.Add (new MWC.BL.Tweet() {
+					Title = tweet["user"]["name"],
+					Content = tweet["text"],
+					RealName = tweet["user"]["name"],
+					Author = tweet["user"]["screen_name"],
+					Published = DateTime.ParseExact(tweet["created_at"], "ddd MMM dd HH:mm:ss +ffff yyyy", new CultureInfo("en-US")),
+					ImageUrl = tweet["user"]["profile_image_url"]
+				});
+			}
+			return tweets;
 		}
 	}
 }
