d99fa4bc5d4bdb7a027ef91969d343ca79539275
olegoid <olegoid91@gmail.com>
2014-01-24 04:18:34 +0400

    [MWC.Core] Remove deprecated comments, add Initialize method, override AllItems property

--- MWC/MWC.Core/SAL/TwitterParser.cs
+++ MWC/MWC.Core/SAL/TwitterParser.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Linq;
-using System.Xml.Linq; // requires System.Xml.Linq added to References
+using System.Xml.Linq;
 using System.Collections.Generic;
 using System.Net;
 using System.Diagnostics;
@@ -10,31 +10,32 @@ using System.Json;
 using System.Globalization;
 
 namespace MWC.SAL
-{	
+{
 	/// <summary>
-	/// Load a twitter Atom feed
+	/// Load a twitter feed
 	/// </summary>
-	/// <remarks>
-	/// Inspired by the RSSRepository from 
-	/// http://softwareandservice.wordpress.com/2009/09/21/building-a-rss-reader-iphone-app-using-monotouch/
-	/// </remarks>
 	public class TwitterParser<Tweet> : FeedParserBase<MWC.BL.Tweet>
-	{	
+	{
+		private List<MWC.BL.Tweet> tweets;
 		private TwitterAuthorizer twitterAuthorizer;
-		//https://dev.twitter.com/docs/api/1/get/search
 		const string fileName = "TwitterFeed.json";
-		//static string url = "http://search.twitter.com/search.atom?q=%40mobileworldlive&show-user=true&rpp=20";
 		const string urlFormat = "http://search.twitter.com/search.atom?q={0}&show-user=true&rpp={1}";
-		
-		public TwitterParser(string url) : base(url, fileName)
+
+		public override List<MWC.BL.Tweet> AllItems {
+			get {
+				return tweets;
+			}
+		}
+
+		public TwitterParser (string url) : base (url, fileName)
 		{
-			twitterAuthorizer = new TwitterAuthorizer ();
+			Initialize ();
 			Debug.WriteLine ("Created new Twitter Repository");
 		}
-		
-		public TwitterParser(string keyWord, string amountOfTweets) : base(String.Format(urlFormat, keyWord, amountOfTweets), fileName)
+
+		public TwitterParser (string keyWord, string amountOfTweets) : base (String.Format (urlFormat, keyWord, amountOfTweets), fileName)
 		{
-			twitterAuthorizer = new TwitterAuthorizer ();
+			Initialize ();
 			Debug.WriteLine ("setting up twitter repository");
 		}
 
@@ -42,40 +43,46 @@ namespace MWC.SAL
 		{
 			var webClient = new WebClient ();
 			Debug.WriteLine ("Get remote json data");
-			webClient.DownloadStringCompleted += (sender, e) =>
-			{
+			webClient.DownloadStringCompleted += (sender, e) => {
 				try {
 					SaveLocal (e.Result);
-					var items = Parse (e.Result);
+					tweets = Parse (e.Result);
 				} catch (Exception ex) {
 					Debug.WriteLine ("ERROR saving downloaded JSON: " + ex);
 				}
-				action();
+				action ();
 			};
 
 			webClient.Headers.Set ("Authorization", string.Format ("{0} {1}", twitterAuthorizer.AuthenticationInfo.TokenType,
 				twitterAuthorizer.AuthenticationInfo.AccessToken));
-			webClient.Encoding = System.Text.Encoding.UTF8;webClient.DownloadStringAsync (new Uri(Constants.TwitterUrl));
+			webClient.Encoding = System.Text.Encoding.UTF8;
+			webClient.DownloadStringAsync (new Uri (Constants.TwitterUrl));
 		}
-		
+
 		protected override List<MWC.BL.Tweet> Parse (string data)
 		{
 			Debug.WriteLine ("Starting Parsing JSON");
 
 			var tweets = new List<MWC.BL.Tweet> ();
-			var statusesJson = JsonValue.Parse (data)["statuses"];
-			foreach(var jsonItem in statusesJson) {
+			var statusesJson = JsonValue.Parse (data) ["statuses"];
+			foreach (var jsonItem in statusesJson) {
 				var tweet = jsonItem as JsonObject;
-				tweets.Add (new MWC.BL.Tweet() {
-					Title = tweet["user"]["name"],
-					Content = tweet["text"],
-					RealName = tweet["user"]["name"],
-					Author = tweet["user"]["screen_name"],
-					Published = DateTime.ParseExact(tweet["created_at"], "ddd MMM dd HH:mm:ss +ffff yyyy", new CultureInfo("en-US")),
-					ImageUrl = tweet["user"]["profile_image_url"]
+				tweets.Add (new MWC.BL.Tweet () {
+					Title = tweet ["user"] ["name"],
+					Content = tweet ["text"],
+					RealName = tweet ["user"] ["name"],
+					Author = tweet ["user"] ["screen_name"],
+					Published = DateTime.ParseExact (tweet ["created_at"], "ddd MMM dd HH:mm:ss +ffff yyyy", new CultureInfo ("en-US")),
+					ImageUrl = tweet ["user"] ["profile_image_url"]
 				});
 			}
 			return tweets;
+		}
+
+		private void Initialize ()
+		{
+			twitterAuthorizer = new TwitterAuthorizer ();
+			tweets = new List<MWC.BL.Tweet> ();
 		}
 	}
 }
