763c3c719ba0cc28e6d6350cf086aac312704f02
Tom Opgenorth <tom@opgenorth.net>
2014-07-18 15:36:56 -0600

    Adding some stuff to the build bash script. #documentation/51

--- CreditCardValidation-UITest/testcloud-submit-example.sh
+++ CreditCardValidation-UITest/testcloud-submit-example.sh
@@ -11,16 +11,20 @@
 export TESTCLOUD_ID=YOUR_API_KEY_HERE
 export DEVICE_ID=YOUR_DEVICE_ID_HERE
 
+# This might have to be updated depending on the version of Xamarin.UITest.Console 
+export XUTCONSOLE=./packages/Xamarin.UITest.Console.0.4.3/tools/xut-console.exe
+
 # You shouldn't have to update these variable.
 export IPA=./CreditCardValidation.iOS/bin/iPhone/Debug/CreditCardvalidationiOS-1.0.ipa
 export TEST_ASSEMBLIES=./CreditCardValidation.Tests/bin/Debug/
-export XUTCONSOLE=./packages/Xamarin.UITest.Console.0.4.0/tools/xut-console.exe
+export DSYM=./CreditCardValidation.iOS/bin/iPhone/Debug/CreditCardValidationiOS.app.dSYM
 
 # Compile a debug .IPA for an iPhone.
 /Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool -v build "--configuration:Debug|iPhone" ./CreditCardValidation.sln
 
-# This is the command line for submitting the C# tests to Test Cloud using xut-console.exe
-mono $XUTCONSOLE submit $IPA $TESTCLOUD_ID --devices $DEVICE_ID --series "iOS" --locale "en_US" --assembly-dir $TEST_ASSEMBLIES
+# (iOS) This is the command line for submitting the C# tests to Test Cloud using xut-console.exe 
+mono $XUTCONSOLE submit $IPA $TESTCLOUD_ID --devices $DEVICE_ID --series "iOS" --locale "en_US" --assembly-dir $TEST_ASSEMBLIES --app-name "Simple Credit Card Validator" --dsym $DSYM
 
-# This is the command line for submitting the Ruby tests to Test Cloud using the test-cloud gem.
-#test-cloud submit $IPA $ID --devices $DEVICE_ID --series "iOS" --locale "en_US"
+# Compile Release APK.
+/usr/bin/xbuild 
+
