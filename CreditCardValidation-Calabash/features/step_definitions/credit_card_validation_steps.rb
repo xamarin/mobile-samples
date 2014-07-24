21c2c7cae6d9e8f611d0eeacbd2b114eab24ade6
jescriba <escribirajoshua@gmail.com>
2014-07-24 11:24:41 -0700

    Minor spacing fix.

--- CreditCardValidation-Calabash/features/step_definitions/credit_card_validation_steps.rb
+++ CreditCardValidation-Calabash/features/step_definitions/credit_card_validation_steps.rb
@@ -1,8 +1,8 @@
 Given(/^I try to validate a credit card number that is (\d+) characters long$/) do |number_of_digits|
   touch("textField marked:'CreditCardNumberField'")
-    wait_for_keyboard
-    keyboard_enter_text("9" * number_of_digits.to_i)
-    touch("button marked:'Validate'")
+  wait_for_keyboard
+  keyboard_enter_text("9" * number_of_digits.to_i)
+  touch("button marked:'Validate'")
 end
 
 Then(/^I should see the error message "(.*?)"$/) do |error_message|
