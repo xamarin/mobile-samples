41afe13b2d0ff5a88925fc3fe9096d07b205930b
Tom Opgenorth <tom@opgenorth.net>
2014-09-22 11:19:59 -0600

    Fix typo in directory names.

--- CreditCardValidation/UITest/CreditCardValidation.Droid/Resources/Resource.designer.cs
+++ CreditCardValidation/UITest/CreditCardValidation.Droid/Resources/Resource.designer.cs
@@ -24,6 +24,10 @@ namespace CreditCardValidation.Droid
 			global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 		}
 		
+		public static void UpdateIdValues()
+		{
+		}
+		
 		public partial class Attribute
 		{
 			
