1bde89318a91d09fac657969778699859d7036eb
Tom Opgenorth <tom@opgenorth.net>
2014-12-18 15:10:51 -0700

    Fix up the broken scenarios (incorrect calabash query).

--- TestCloud/CreditCardValidation/CreditCardValidation-Calabash/features/credit_card_validation.feature
+++ TestCloud/CreditCardValidation/CreditCardValidation-Calabash/features/credit_card_validation.feature
@@ -1,7 +1,8 @@
 Feature: Credit card validation.
   Credit card numbers must be exactly 16 characters.
 
-  Scenario: Credit card number is too short
+  Scenario: Credit card number is too short (using redefined steps)
+    # this is an example of using predefined steps - not really recommended.
     Given I use the native keyboard to enter "123456" into text field number 1
     And I touch the "ValidateButton" button
     Then I see the text "Credit card number is too short."
@@ -13,3 +14,7 @@ Feature: Credit card validation.
   Scenario: Credit card number is an empty string
       Given I touch the "ValidateButton" button
       Then I should see the error message "This is not a credit card number."
+
+  Scenario: Credit card number is exactly 16 characters
+    Given I try to validate a credit card number that is 16 characters long
+    Then I should see the Valid Credit Card screen with success message
--- TestCloud/CreditCardValidation/CreditCardValidation-Calabash/features/step_definitions/credit_card_validation_steps.rb
+++ TestCloud/CreditCardValidation/CreditCardValidation-Calabash/features/step_definitions/credit_card_validation_steps.rb
@@ -1,3 +1,4 @@
+
 Given(/^I try to validate a credit card number that is (\d+) characters long$/) do |number_of_digits|
   touch("textField marked:'CreditCardTextField'")
   wait_for_keyboard
@@ -6,6 +7,15 @@ Given(/^I try to validate a credit card number that is (\d+) characters long$/) 
 end
 
 Then(/^I should see the error message "(.*?)"$/) do |error_message|
-  text_view = query("textView marked:'ErrorMessagesTextField' {text CONTAINS '#{error_message}'}")
-  raise "The error message '#{error_message}' is not visible in the View." unless text_view.any?
+  wait_for_elements_exist(["label id:'ErrorMessagesTextField'"], :timeout=>5)
+
+  label_view = query("label id:'ErrorMessagesTextField' {text CONTAINS '#{error_message}'}")
+  raise "The error message '#{error_message}' is not visible in the View." unless label_view.any?
+end
+
+Then(/^I should see the Valid Credit Card screen with success message$/) do
+  wait_for_elements_exist(["UINavigationBar marked:'Valid Credit Card'"], :timeout => 30)
+
+  label_view = query("label marked:'CreditCardIsValidLabel' {text CONTAINS 'The credit card number is valid!'}")
+  raise "The success messages was not displayed" unless label_view.any?
 end
