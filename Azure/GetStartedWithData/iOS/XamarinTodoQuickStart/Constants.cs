5fe11e1df6b15b8f1391a866e6757f7909d763c9
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-02-20 20:01:52 -0800

    [azure] move to github, convert iOS to unified

---
+++ Azure/GetStartedWithData/iOS/XamarinTodoQuickStart/Constants.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace XamarinTodoQuickStart
+{
+    public static class Constants
+    {
+        // TODO: Configure the MobileServiceClient to communicate with your mobile service by
+        // uncommenting the following code and replacing AppUrl & AppKey with values from  
+        // your mobile service, which are obtained from the Windows Azure Management Portal.
+        // Do this after you add a reference to the Mobile Services client to your project.
+
+        // Azure app specific URL and key
+		public const string ApplicationURL = @"https://YOUR_APP_NAME.azure-mobile.net/";
+		public const string ApplicationKey = @"YOUR_APP_KEY";
+    }
+}
+
---
+++ Azure/NotificationHubs/Android/AzureCloudCode/read.js
@@ -0,0 +1,4 @@
+function read(query, user, request) {
+    // query.where({ userId: user.userId });
+    request.execute();
+}
