a8ea3c90dec5cff968ffa0c0cfcf84885234b172
Victor Chelaru <VicChelaru@gmail.com>
2016-03-13 14:19:19 -0600

    First upload of CCRenderTexture example.

---
+++ CCRenderTexture/README.md
@@ -0,0 +1,12 @@
+CCRenderTarget Example Project
+================
+
+This solution contains an iOS and Android CocosSharp project which shows how to work with the CCRenderTexture object. This app highlights the performance benefits of using CCRenderTexture in certain situations. It also shows how CCRenderTexture can be used to apply transparency to an entire entity.
+
+The `useRenderTarget` and `opacity` variables can be modified to see the impact of CCRenderTexture.
+
+This demo is built against CocosSharp 1.7.1.
+
+Authors
+-------
+Victor Chelaru
---
+++ CCRenderTexture/RenderTextureExample/RenderTextureExample.Common/GameLayer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using CocosSharp;
+
+namespace RenderTextureExample
+{
+	public class GameLayer : CCLayerColor
+	{
+		public GameLayer () : base (CCColor4B.Black)
+		{
+
+		}
+
+		protected override void AddedToScene ()
+		{
+            base.AddedToScene();
+
+            GameView.Stats.Enabled = true;
+
+            const bool useRenderTextures = true;
+            const byte opacity = 255;
+
+            const float positionIncrement = 140;
+
+            Card card = new Card();
+            card.ColorIconTexture = CCTextureCache.SharedTextureCache.AddImage("GreenIcon.png");
+            card.MonsterTexture = CCTextureCache.SharedTextureCache.AddImage("GreenGuy.png");
+            card.DescriptionText = "Does extra damage to water units";
+            card.MonsterName = "Grobot";
+            card.PositionX = 20;
+            card.PositionY = 100;
+            card.UsesRenderTexture = useRenderTextures;
+            card.Opacity = opacity;
+            this.AddChild(card);
+
+            card = new Card();
+            card.ColorIconTexture = CCTextureCache.SharedTextureCache.AddImage("BlueIcon.png");
+            card.MonsterTexture = CCTextureCache.SharedTextureCache.AddImage("BlueGuy.png");
+            card.DescriptionText = "Takes reduced damage from earth attacks";
+            card.MonsterName = "Bluchirp";
+            card.PositionX = 20 + positionIncrement;
+            card.PositionY = 100;
+            card.UsesRenderTexture = useRenderTextures;
+            card.Opacity = opacity;
+            this.AddChild(card);
+
+            card = new Card();
+            card.ColorIconTexture = CCTextureCache.SharedTextureCache.AddImage("OrangeIcon.png");
+            card.MonsterTexture = CCTextureCache.SharedTextureCache.AddImage("OrangeGuy.png");
+            card.DescriptionText = "High chance of preemptive attack";
+            card.MonsterName = "Orange-U-Glad";
+            card.PositionX = 20 + positionIncrement * 2;
+            card.PositionY = 100;
+            card.UsesRenderTexture = useRenderTextures;
+            card.Opacity = opacity;
+            this.AddChild(card);
+
+        }
+	}
+}
