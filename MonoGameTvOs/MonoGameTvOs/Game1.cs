2d9940c8985a24b14f27801be3993ea6aa66c324
Victor Chelaru <vchelaru@I.local>
2016-05-01 11:57:43 -0600

    Fist checkin of MonoGame tvOS sample.

---
+++ MonoGameTvOs/MonoGameTvOs/Game1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace MonoGameTvOs
+{
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+
+		public Game1 ()
+		{
+			graphics = new GraphicsDeviceManager (this);
+			graphics.IsFullScreen = true;
+		}
+
+		protected override void Initialize ()
+		{
+			base.Initialize ();
+		}
+
+		protected override void Update (GameTime gameTime)
+		{
+			base.Update (gameTime);
+		}
+
+		protected override void Draw (GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+
+			base.Draw (gameTime);
+		}
+	}
+}
+
---
+++ MonoGameTvOs/README.md
@@ -0,0 +1,12 @@
+MonoGame tvOS Project
+================
+
+This solution contains an empty MonoGame tvOS project. It contains a pre-compiled .dll (not from Nuget) since MonoGame does not currently distribute a tvOS .dll.
+
+This project contains an empty Game class with a single call to GraphicsDevice.Clear, setting the background color to Color.CornflowerBlue.
+
+This demo includes MonoGame.Framework.dll which was compiled using MonoGame 3.5 source.
+
+Authors
+-------
+Victor Chelaru
