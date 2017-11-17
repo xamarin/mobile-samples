0d0984d0e8a4d7210830adfbad6e43bffdd74f0c
Amy Burns <amy.burns87@gmail.com>
2017-11-17 15:45:17 -0500

    Adding Live Player sample

---
+++ LivePlayer/LiveView/LiveView/LiveView/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LiveView
+{
+	public partial class MainPage : ContentPage
+	{
+		int clickTotal = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		void OnButtonClicked(object sender, EventArgs e)
+		{
+			clickTotal += 1;
+			label.Text = String.Format("{0} button click{1}",
+									   clickTotal, clickTotal == 1 ? "" : "s");
+		}
+	}
+	}
+}
---
+++ LivePlayer/LiveView/readme.md
@@ -0,0 +1,4 @@
+Live View
+==========
+
+This is a basic sample that will allow you to add and remove Xaml to explore the Xamarin Live Player's Live View capabilities.
