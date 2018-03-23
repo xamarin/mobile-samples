b971084ba94489dece0c2a80fe15561e7bfab96f
Charles Petzold <chape@microsoft.com>
2018-03-23 14:52:04 -0400

    [WeatherApp] from VS 2017 with .NET Std lib

---
+++ WeatherApp/WeatherApp/DataService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+// using Newtonsoft.Json;
+
+
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace WeatherApp
+{
+    public class DataService
+    {
+        public static async Task<dynamic> getDataFromService(string queryString)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(queryString);
+
+            dynamic data = null;
+            if (response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                var x = new DataContractJsonSerializer(typeof(object));
+
+           //     StringReader reader = new StringReader(json);
+
+                MemoryStream stream = new MemoryStream();
+                StreamWriter writer = new StreamWriter(stream);
+                writer.Write(json);
+                writer.Flush();
+                stream.Position = 0;
+                
+                data = x.ReadObject(stream);
+
+
+          //      data = JsonConvert.DeserializeObject(json);
+            }
+
+            return data;
+        }
+    }
+}
---
+++ xWeather/WeatherApp/WeatherApp.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace WeatherApp.WinPhone
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private async void GetWeatherButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                if (weather != null)
+                {
+                    locationText.Text = weather.Title;
+                    tempText.Text = weather.Temperature;
+                    windText.Text = weather.Wind;
+                    visibilityText.Text = weather.Visibility;
+                    humidityText.Text = weather.Humidity;
+                    sunriseText.Text = weather.Sunrise;
+                    sunsetText.Text = weather.Sunset;
+
+                    weatherBtn.Content = "Search Again";
+                }
+            }
+        }
+    }
+}
