7b7459fc9853e8ee751adfd201909419898212dc
Craig Dunn <craig.dunn@conceptdevelopment.net>
2018-07-05 17:59:41 -0700

    [Phoneword] update configuration for latest platforms
    fixes https://github.com/xamarin/mobile-samples/issues/34

--- Phoneword/PhonewordiOS/PhonewordiOSViewController.designer.cs
+++ Phoneword/PhonewordiOS/PhonewordiOSViewController.designer.cs
@@ -1,42 +1,46 @@
 // WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 using UIKit;
 
 namespace PhonewordiOS
 {
-	[Register ("PhonewordiOSViewController")]
-	partial class PhonewordiOSViewController
-	{
-		[Outlet]
-		UITextField PhoneNumberText { get; set; }
+    [Register ("PhonewordiOSViewController")]
+    partial class PhonewordiOSViewController
+    {
+        [Outlet]
+        UITextField PhoneNumberText { get; set; }
 
-		[Outlet]
-		UIButton TranslateButton { get; set; }
 
-		[Outlet]
-		UIButton CallButton { get; set; }
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (PhoneNumberText != null) {
-				PhoneNumberText.Dispose ();
-				PhoneNumberText = null;
-			}
+        [Outlet]
+        UIButton TranslateButton { get; set; }
 
-			if (TranslateButton != null) {
-				TranslateButton.Dispose ();
-				TranslateButton = null;
-			}
 
-			if (CallButton != null) {
-				CallButton.Dispose ();
-				CallButton = null;
-			}
-		}
-	}
+        [Outlet]
+        UIButton CallButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (CallButton != null) {
+                CallButton.Dispose ();
+                CallButton = null;
+            }
+
+            if (PhoneNumberText != null) {
+                PhoneNumberText.Dispose ();
+                PhoneNumberText = null;
+            }
+
+            if (TranslateButton != null) {
+                TranslateButton.Dispose ();
+                TranslateButton = null;
+            }
+        }
+    }
 }
--- Phoneword/README.md
+++ Phoneword/README.md
@@ -1,11 +1,12 @@
 Phoneword
 =========
 
-Very simple example of sharing C# code across four platforms: iOS, Android, Windows Phone 7 and Windows 8. 
+Very simple example of sharing C# code across iOS and Android.
 Also includes a command-line version as a 'test harness'.
 
 App functionality is to convert telephone number 'words' into digits (eg 1-800-FLOWERS to the actual phone number).
 
+_Deprecated versions for Windows Phone 7 and Windows 8 can be found in the GitHub repo, but require older versions of Visual Studio (earlier than 2017) to open and run._
 
 Authors
 -------
