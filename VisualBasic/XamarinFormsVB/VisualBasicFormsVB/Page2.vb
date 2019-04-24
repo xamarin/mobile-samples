d22cb870030f5bc84f9d321e70e3290dd8f61adc
Craig Dunn <craig.dunn@conceptdevelopment.net>
2019-04-24 14:28:04 -0700

    [vb] rebuild Xamarin.Forms Visual Basic example

---
+++ VisualBasic/XamarinFormsVB/VisualBasicFormsVB/Page2.vb
@@ -0,0 +1,18 @@
+﻿Imports Xamarin.Forms
+
+Public Class Page2
+    Inherits ContentPage
+
+    Public Sub New()
+        Dim label = New Label With {.HorizontalTextAlignment = TextAlignment.Center,
+                                    .FontSize = Device.GetNamedSize(NamedSize.Medium, GetType(Label)),
+                                    .Text = "Visual Basic ContentPage"}
+
+        Dim stack = New StackLayout With {
+            .VerticalOptions = LayoutOptions.Center
+        }
+        stack.Children.Add(label)
+
+        Content = stack
+    End Sub
+End Class
